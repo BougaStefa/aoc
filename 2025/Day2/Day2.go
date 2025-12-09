afdac2a565a1886b85a4f973de7500f3890215e3
BougaStefa <BougaStefa@gmail.com>
2025-12-09 00:19:05 +0000

    Day2 Part 2

--- 2025/Day2/Day2.go
+++ 2025/Day2/Day2.go
@@ -19,6 +19,7 @@ func main() {
 	ranges := strings.Split(s, ",")
 
 	partOne(ranges)
+	partTwo(ranges)
 }
 
 func partOne(ranges []string) {
@@ -54,5 +55,44 @@ func isRepeat(s string) bool {
 	if s[:mid] == s[mid:] {
 		return true
 	}
+	return false
+}
+
+func partTwo(ranges []string) {
+	totalOfIDs := 0
+	for _, pair := range ranges {
+		left, right, ok := strings.Cut(pair, "-")
+		if !ok {
+			log.Fatal("No separator found")
+		}
+
+		firstId, err := strconv.Atoi(left)
+		if err != nil {
+			log.Fatal(err)
+		}
+		secondId, err := strconv.Atoi(right)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i := firstId; i <= secondId; i++ {
+			currentNum := strconv.Itoa(i)
+			if !isRepeatTwice(currentNum) {
+				continue
+			}
+			totalOfIDs += i
+		}
+	}
+	fmt.Println("Total of IDs:", totalOfIDs)
+}
+
+func isRepeatTwice(s string) bool {
+	dupeString := (s + s)[1 : len(s)*2-1]
+
+	if strings.Contains(dupeString, s) {
+		fmt.Printf("%v meets the criteria\n", s)
+		return true
+	}
+
 	return false
 }
