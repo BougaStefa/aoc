bcfee80acfd761f21ec8afd082b2b4b4dc3a95ce
BougaStefa <BougaStefa@gmail.com>
2025-12-10 21:27:22 +0000

    Day3 Part2

--- 2025/Day3/Day3.go
+++ 2025/Day3/Day3.go
@@ -16,7 +16,8 @@ func main() {
 
 	banks := strings.Split(strings.TrimSpace(string(data)), "\n")
 
-	partOne(banks)
+	// partOne(banks)
+	partTwo(banks)
 }
 
 func partOne(banks []string) {
@@ -26,6 +27,101 @@ func partOne(banks []string) {
 		num2, _ := findBiggestNum(bank[index+1:])
 
 		joltage, err := strconv.Atoi(num1 + num2)
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalJoltage += joltage
+	}
+	fmt.Println(totalJoltage)
+}
+
+func partTwo(banks []string) {
+	totalJoltage := 0
+	for _, bank := range banks {
+
+		num1, index := findBiggestNum(bank[:len(bank)-11])
+		offset := index + 1
+
+		end := len(bank) - 10
+		if end <= offset {
+			log.Fatal("no elements")
+		}
+		num2, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 9
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num3, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 8
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num4, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 7
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num5, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 6
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num6, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 5
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num7, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 4
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num8, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 3
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num9, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 2
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num10, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank) - 1
+		if end == offset {
+			log.Fatal("no elements")
+		}
+		num11, localIndex := findBiggestNum(bank[offset:end])
+		offset = localIndex + 1 + offset
+
+		end = len(bank)
+		if end <= offset {
+			log.Fatal("no elements")
+		}
+		num12, _ := findBiggestNum(bank[offset:])
+
+		joltage, err := strconv.Atoi(
+			num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10 + num11 + num12,
+		)
+		fmt.Printf("DEBUG: joltage was %v\n", joltage)
 		if err != nil {
 			log.Fatal(err)
 		}
