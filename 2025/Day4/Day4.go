71edea2c5c13f43c9b1eff64d3ca4cf465b1930a
BougaStefa <BougaStefa@gmail.com>
2025-12-12 17:41:41 +0000

    Day4 Part2

--- 2025/Day4/Day4.go
+++ 2025/Day4/Day4.go
@@ -14,6 +14,7 @@ func main() {
 	}
 	grid := strings.Split(strings.TrimRight(string(data), "\n"), "\n")
 	partOne(grid)
+	partTwo(grid)
 }
 
 func partOne(grid []string) {
@@ -62,4 +63,62 @@ func partOne(grid []string) {
 		}
 	}
 	fmt.Println(totalRolls)
+}
+
+func partTwo(grid []string) {
+	totalRolls := 0
+	prevRolls := 0
+	for {
+		prevRolls = totalRolls
+		for i, row := range grid {
+			runeRow := []rune(row)
+			for j, item := range runeRow {
+				totalAdjacents := 0
+				if item == '@' {
+					// Check left if possible
+					if j != 0 && row[j-1] == '@' {
+						totalAdjacents++
+					}
+					// Check right if possible
+					if j != len(row)-1 && row[j+1] == '@' {
+						totalAdjacents++
+					}
+					// Check above if possible
+					if i != 0 && grid[i-1][j] == '@' {
+						totalAdjacents++
+					}
+					// Check below if possible
+					if i != len(grid)-1 && grid[i+1][j] == '@' {
+						totalAdjacents++
+					}
+					// Check top left if possible
+					if i != 0 && j != 0 && grid[i-1][j-1] == '@' {
+						totalAdjacents++
+					}
+					// Check top right if possible
+					if i != 0 && j != len(row)-1 && grid[i-1][j+1] == '@' {
+						totalAdjacents++
+					}
+					// Check bottom left if possible
+					if i != len(grid)-1 && j != 0 && grid[i+1][j-1] == '@' {
+						totalAdjacents++
+					}
+					// Check bottom right if possible
+					if i != len(grid)-1 && j != len(row)-1 && grid[i+1][j+1] == '@' {
+						totalAdjacents++
+					}
+
+					if totalAdjacents < 4 {
+						totalRolls++
+						runeRow[j] = 'x'
+					}
+				}
+			}
+			grid[i] = string(runeRow)
+		}
+		if prevRolls == totalRolls {
+			fmt.Println(totalRolls)
+			return
+		}
+	}
 }
