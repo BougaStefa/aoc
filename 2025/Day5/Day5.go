0e0ea0e3c683dbcdc83d3bdc5cd76681221dc480
BougaStefa <BougaStefa@gmail.com>
2025-12-13 00:16:30 +0000

    Day5 Part2

--- 2025/Day5/Day5.go
+++ 2025/Day5/Day5.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -27,6 +28,7 @@ func main() {
 	}
 
 	partOne(freshRanges, ingredients)
+	partTwo(freshRanges)
 }
 
 func partOne(ranges []string, ingredients []string) {
@@ -51,4 +53,75 @@ func isInRange(item int, ranges []string) bool {
 		}
 	}
 	return isInRange
+}
+
+// This one is a bit demented would take 25 days to run (should be right though)
+// func partTwo(freshRange []string) {
+// 	totalInRange := 0
+// 	m := make(map[int]string)
+// 	for i, ran := range freshRange {
+// 		fmt.Println("We are at iterration: ", i)
+// 		splIndex := strings.Index(ran, "-")
+// 		floor, _ := strconv.Atoi(ran[:splIndex])
+// 		ceil, _ := strconv.Atoi(ran[splIndex+1:])
+// 		for j := floor; j <= ceil; j++ {
+// 			fmt.Println("We are in neted loop iterration: ", j)
+// 			_, ok := m[j]
+// 			if !ok {
+// 				totalInRange++
+// 				m[j] = ""
+// 			}
+// 		}
+// 	}
+// 	fmt.Println(totalInRange)
+// }
+
+func partTwo(freshRange []string) {
+	type ranges struct {
+		Start int
+		End   int
+	}
+	totalInRange := 0
+	allRanges := []ranges{}
+	for _, ran := range freshRange {
+		splIndex := strings.Index(ran, "-")
+		floor, _ := strconv.Atoi(ran[:splIndex])
+		ceil, _ := strconv.Atoi(ran[splIndex+1:])
+		allRanges = append(allRanges, ranges{Start: floor, End: ceil})
+	}
+
+	slices.SortFunc(allRanges, func(a, b ranges) int {
+		if a.Start < b.Start {
+			return -1
+		}
+		if a.Start > b.Start {
+			return 1
+		}
+		if a.End < b.End {
+			return -1
+		}
+		if a.End > b.End {
+			return 1
+		}
+		return 0
+	})
+
+	curStart := allRanges[0].Start
+	curEnd := allRanges[0].End
+	for i, ran := range allRanges {
+		if i == 0 {
+			continue
+		}
+		if ran.Start <= curEnd+1 {
+			if ran.End > curEnd {
+				curEnd = ran.End
+			}
+		} else {
+			totalInRange += curEnd - curStart + 1
+			curStart = ran.Start
+			curEnd = ran.End
+		}
+	}
+	totalInRange += curEnd - curStart + 1
+	fmt.Println(totalInRange)
 }
